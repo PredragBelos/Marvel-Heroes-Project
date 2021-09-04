1f2ddc1cc03e1ba42d72c70c8d89680f55303cef
PredragBelos <predragbelos@gmail.com>
2021-09-04 13:53:45 +0200

    Task 4 - Create search bar component

--- src/components/Hero Cards/heroCards.jsx
+++ src/components/Hero Cards/heroCards.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { SearchBar } from '../Search bar/searchBar';
 import './heroCards.css';
 
 export const HeroCards = () => {
     return(
         <div className="heroCards">
-            Cards
+            <SearchBar/>
         </div>
     );
 }
