4c58455bb5ed8de0738c7701adad476dd5fc6bd7
PredragBelos <predragbelos@gmail.com>
2021-09-04 16:47:08 +0200

    Task 3.1 - Show caracter data into Cards components

--- src/components/Hero Cards/heroCards.jsx
+++ src/components/Hero Cards/heroCards.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { caracterURL } from '../../data/publicData';
+import { getHeroData } from '../../services/createHeroesList';
+import { Card } from '../Card/card';
 import './heroCards.css';
 
 export const HeroCards = () => {
-    return(
+
+    const [heroData, setHeroData] = useState([]);
+
+    useEffect(() => {
+        getHeroData(caracterURL).then(result => {
+            setHeroData(result);
+        })
+    }, [])
+
+    return (
         <div className="heroCards">
-            Cards
+            {
+                heroData.map((hero, index) => {
+                    return <Card name={hero.name} image={hero.image} key={index} />
+                })
+            }
         </div>
     );
 }
