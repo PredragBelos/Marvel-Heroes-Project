9e9ad787f22fa0132ce9abb627794a026cda6c1a
PredragBelos <predragbelos@gmail.com>
2021-09-04 14:45:17 +0200

    Task 5 - Created component for MyTeam component

--- src/components/My Team/myTeam.jsx
+++ src/components/My Team/myTeam.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { TeamUser } from '../Team user/teamUser';
 import './myTeam.css';
 
 export const MyTeam = () => {
     return(
         <div className="myTeam">
-           My team 
+
+            <h3>My Team</h3>
+           <TeamUser/>
+           <TeamUser/>
+           <TeamUser/>
+           <TeamUser/>
         </div>
     );
 }
